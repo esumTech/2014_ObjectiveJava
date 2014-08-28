e66468f1ecfec4998eb0c38f6163548060f86a7e
esumTech <bigdown@esumtech.com>
2014-08-28 10:35:26 +0900

    Main 클래스 수정합니다. Change-Id: I2405b8aa9d20dc3f122ec2ba0f5b288e63d10342 Signed-off-by: YunChang.Lee <cosmos.hurukku@gmail.com>

--- 이윤창/01_HelloWorld/src/com/esum/java/HelloWorld.java
+++ 이윤창/01_HelloWorld/src/com/esum/java/HelloWorld.java
@@ -1,10 +1,12 @@
 package com.esum.java;
 
+import java.util.Date;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-
+		System.out.println("HelloWorld:"+new Date());
+		
 	}
 
 }
